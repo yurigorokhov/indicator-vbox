55a1cb5a5e9ea46cce13401118bf8fc64e1d33db
Yuri Gorokhov <yurigorokhov@gmail.com>
2011-02-08 10:44:09 -0800

    Updated installer with new file.

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ INSTALL_DIR=/usr/local/bin
 
 install -m 644 ./images/* /usr/share/pixmaps/
 install -m 644 ./src/indicator-vbox.py $INSTALL_DIR/indicator-vbox
+install -m 644 ./src/VBox.pyc $INSTALL_DIR/
 chmod +x $INSTALL_DIR/indicator-vbox
