9409f0025c60cf5806b7ddfaa66b14e93e1fbd1a
Yuri Gorokhov <yurigorokhov@gmail.com>
2011-02-08 14:09:20 -0800

    Added --uninstall option and updated README

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 
 INSTALL_DIR=/usr/local/bin
 
-install -m 644 ./images/* /usr/share/pixmaps/
-install -m 644 ./src/indicator-vbox.py $INSTALL_DIR/indicator-vbox
-install -m 644 ./src/VBox.pyc $INSTALL_DIR/
-chmod +x $INSTALL_DIR/indicator-vbox
+if [ "$1" = "--uninstall" ]; then
+	rm -r -f /usr/share/pixmaps/VBox-gray.png
+	rm -r -f $INSTALL_DIR/indicator-vbox
+	rm -r -f $INSTALL_DIR/VBox.pyc
+else
+	install -m 644 ./images/* /usr/share/pixmaps/
+	install -m 644 ./src/indicator-vbox.py $INSTALL_DIR/indicator-vbox
+	install -m 644 ./src/VBox.pyc $INSTALL_DIR/
+	chmod +x $INSTALL_DIR/indicator-vbox
+fi
+
